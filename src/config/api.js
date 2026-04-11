eae9a14590bc1a77c59f96610bd17a3432fb108d
Evaudo <egallego@hotmail.comm>
2026-04-11 02:47:28 -0300

    5.6.2 exports e imports cli y ventas

--- src/config/api.js
+++ src/config/api.js
@@ -26,7 +26,7 @@ if (import.meta.env.PROD) {
 }
 
 // Front Version ************************
-export const FRONTEND_VERSION = '5.2.1';
+export const FRONTEND_VERSION = '5.6.2';
 // **************************************
 
 // Export API_BASE_URL for components that need direct URL access
